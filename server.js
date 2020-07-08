9d1ccb970db0534bf12ef8a6bf1d61ebdd41ab36
Jefte Souza <jeftejsouza@gmail.com>
2020-07-07 23:58:59 -0300

    criacao do Procfile

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //import mongoose from './model/db.js';
 import express from 'express';
 import { router as roteador } from './router/routerAccounts.js';
+import cors from "cors";
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -8,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 
-
+app.use(cors());
 app.use('/', roteador);
 
 app.listen(port, () => {
